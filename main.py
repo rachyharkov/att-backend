b4c7cba634307ec1cd0f1871aab1196a9d01131c
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-27 11:08:48 +0700

    Login Route with validation

--- main.py
+++ main.py
@@ -1,43 +1,76 @@
-from flask import Flask,render_template,request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL 
 import os
+import jwt
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DATABASE')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
  
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return 'asdadsasad'
+    return 'mantaapp'
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
+
     if request.method == 'GET':
         return "Login via the login Form"
-     
+
     if request.method == 'POST':
-        username = request.form['usernamenya']
-        idaccount = request.form['idaccountnya']
+        username = request.json['usernamenya']
+        password = request.json['passwordnya']
 
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM account WHERE username = %s AND id_account = %s", (username, idaccount))
+        cursor.execute("SELECT * FROM account WHERE username = %s", (username,)) # <-- this is the problem, I think it's because of the comma
         data = cursor.fetchone()
 
         mysql.connection.commit()
         cursor.close()
         
-        print('I FOUND : ' + str(data))
-        print('===============================')
-        print('USERNAME : ' + str(data[2]))
-        print('ID ACCOUNT : ' + str(data[1]))
-        print('===============================')
+        resp = {
+            'id_account' : '',
+            'status' : '',
+            'message': '',
+            'token': ''
+        }
+
+        # print('I FOUND : ' + str(data))
 
         if data is None:
-            return "Invalid username or password"
+            resp['status'] = 'failed'
+            resp['message'] = 'Anda tidak terdaftar'
+            return resp
         else:
-            return "Login Success"
+            if password == data[3] and username == data[2]:
+
+                token = jwt.encode({
+                    'user' : username, 
+                    'exp' : 'forever'
+                }, app.config['SECRET_KEY'])
+                
+                print('I FOUND : ' + str(data))
+                print('===============================')
+                print('USERNAME : ' + str(data[2]))
+                print('ID ACCOUNT : ' + str(data[1]))
+                print('===============================')
+
+                resp['id_account'] = data[0]
+                resp['status'] = 'success'
+                resp['message'] = 'Anda berhasil login'
+                resp['token'] = token
+                return jsonify(resp)
+            else:
+                resp['status'] = 'failed'
+                resp['message'] = 'Anda terdaftar, tapi data login salah, silahkan coba lagi'
+                return jsonify(resp)
